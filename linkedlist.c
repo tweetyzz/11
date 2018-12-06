707824ec60905622c469aac7bb97f79d3e21f776
tweety <ekak9380@gmail.com>
2018-12-06 15:41:28 +0900

    11.2

--- linkedlist.c
+++ linkedlist.c
@@ -1,5 +1,5 @@
-
-
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct linknd{
 	
@@ -15,3 +15,23 @@ typedef struct linknd{
 //linknd_t nd;
 
  static linknd_t *list; //파일 내부에서만 사용할 전역 변수 
+ 
+ linknd_t* create_node(int value){
+ 	linknd_t*ndptr;
+ 	
+ 	//동적 메모리 할당 & 정수값 저장 
+	ndptr = (linknd_t*)malloc( sizeof(linknd_t)/*구조체의 크기*/);
+	if(ndptr == NULL)
+	{
+		printf("ERROR\n");
+		return NULL;
+	}
+	
+	ndptr->data =value;
+	
+	ndptr->next =NULL; //초기화 
+ 	
+ 	return ndptr;
+ 	
+ 	
+ }
